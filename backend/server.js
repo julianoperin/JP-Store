440071f16a758fc2a05da13db94210f3878d73eb
‘Juliano <julianoperins@gmail.com>
2021-02-17 18:08:03 -0500

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// app.get("/", (res, req) => {
+//   res.send("Working!");
+//   console.log("It's working!");
+// });
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
