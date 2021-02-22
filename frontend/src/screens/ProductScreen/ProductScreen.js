6da7d3e881c29219cf993b68976c8d7d020c22f6
‘Juliano <julianoperins@gmail.com>
2021-02-22 13:50:57 -0500

    Started styling the product screen

--- frontend/src/screens/ProductScreen/ProductScreen.js
+++ frontend/src/screens/ProductScreen/ProductScreen.js
@@ -3,8 +3,48 @@ import "./ProductScreen.scss";
 
 const ProductScreen = () => {
   return (
-    <div>
-      <h1>ProductScreen</h1>
+    <div className="productscreen">
+      <div className="productscreen__left">
+        <div className="left__image">
+          <img
+            src="https://images.unsplash.com/photo-1605787020600-b9ebd5df1d07?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1463&q=80"
+            alt="product"
+          />
+        </div>
+
+        <div className="left__info">
+          <p className="left__name">Product One</p>
+          <p>Price: $455</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic,
+            voluptas.
+          </p>
+        </div>
+      </div>
+      <div className="productscreen__right">
+        <div className="right__info">
+          <p>
+            Price: <span>$455</span>
+          </p>
+          <p>
+            {" "}
+            Status: <span>In Stock</span>
+          </p>
+
+          <p>
+            Quantity
+            <select>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+            </select>
+          </p>
+          <p>
+            <button type="button">Add To Cart</button>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
