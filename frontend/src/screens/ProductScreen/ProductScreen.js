95248905fbcae59325a00997396b686e10226870
‘Juliano <julianoperins@gmail.com>
2021-02-23 14:39:17 -0500

    displayed the correct info about the products on the product screen

--- frontend/src/screens/ProductScreen/ProductScreen.js
+++ frontend/src/screens/ProductScreen/ProductScreen.js
@@ -23,47 +23,55 @@ const ProductScreen = ({ match, history }) => {
 
   return (
     <div className="productscreen">
-      <div className="productscreen__left">
-        <div className="left__image">
-          <img
-            src="https://images.unsplash.com/photo-1605787020600-b9ebd5df1d07?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1463&q=80"
-            alt="product"
-          />
-        </div>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h2>Error</h2>
+      ) : (
+        <>
+          <div className="productscreen__left">
+            <div className="left__image">
+              <img src={product.imageUrl} alt={product.name} />
+            </div>
 
-        <div className="left__info">
-          <p className="left__name">Product One</p>
-          <p>Price: $455</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic,
-            voluptas.
-          </p>
-        </div>
-      </div>
-      <div className="productscreen__right">
-        <div className="right__info">
-          <p>
-            Price: <span>$455</span>
-          </p>
-          <p>
-            {" "}
-            Status: <span>In Stock</span>
-          </p>
+            <div className="left__info">
+              <p className="left__name">{product.name}</p>
+              <p>
+                {" "}
+                <b> Price:</b> ${product.price}
+              </p>
+              <p>
+                {" "}
+                <b> Description:</b> {product.description}
+              </p>
+            </div>
+          </div>
+          <div className="productscreen__right">
+            <div className="right__info">
+              <p>
+                Price: <span>$455</span>
+              </p>
+              <p>
+                {" "}
+                Status: <span>In Stock</span>
+              </p>
 
-          <p>
-            Quantity
-            <select>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-            </select>
-          </p>
-          <p>
-            <button type="button">Add To Cart</button>
-          </p>
-        </div>
-      </div>
+              <p>
+                Quantity
+                <select>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                </select>
+              </p>
+              <p>
+                <button type="button">Add To Cart</button>
+              </p>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
