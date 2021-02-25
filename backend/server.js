dd043888028082d5ef5270b513648eca6b9597db
‘Juliano <julianoperins@gmail.com>
2021-02-25 11:30:09 -0500

    make it better

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 require("dotenv").config();
 const connectDB = require("./config/db");
 const productRoutes = require("./routes/productRoutes");
@@ -10,6 +11,19 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
+
+//! Serve the front end
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api running!");
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
