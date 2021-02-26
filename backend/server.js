d79b33f0bfb25ffd36155a19576327662e57937e
‘Juliano <julianoperins@gmail.com>
2021-02-26 11:22:44 -0500

    fixed the folder path in server.js

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,10 @@ app.use("/api/products", productRoutes);
 
 //! Serve the front end
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
